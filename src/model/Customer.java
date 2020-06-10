54b7a747c29428f6723054b56d0c0f37952f9a6a
Tazurkaev <taz19340@spengergasse.at>
2020-06-10 13:06:33 +0200

    add double credit

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -4,26 +4,35 @@ public class Customer {
 
 	private int customerID;
 	private String firstName;
+	private double credit;
 	
-	public Customer(int customerID, String firstName) {
+	public Customer(int customerID, String firstName, double credit) {
 		setCustomerID(customerID);
 		setFirstName(firstName);
+		setCredit(credit);
+	}
+
+	public double getCredit() {
+		return credit;
 	}
 
 	public int getCustomerID() {
 		return customerID;
 	}
 
-	public void setCustomerID(int customerID) {
-		this.customerID = customerID;
-	}
-
 	public String getFirstName() {
 		return firstName;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
 	}
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
+	
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
 }
